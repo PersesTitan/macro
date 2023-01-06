a061472a4f401619972c5bd6c8f2c00a84f4e49e
PersesTitan <torampersesTitan@gmail.com>
2023-01-06 19:04:56 +0900

    update ButtonTool

--- src/main/java/org/macro/tool/ButtonTool.java
+++ src/main/java/org/macro/tool/ButtonTool.java
@@ -1,6 +1,6 @@
 package org.macro.tool;
 
-import org.macro.keys.Special;
+import org.macro.keys.Option;
 
 import java.awt.*;
 
@@ -10,14 +10,32 @@ public interface ButtonTool {
     void press(Robot robot);
     void release(Robot robot);
 
+    static void click(Robot robot, int key) {
+        robot.keyPress(key);
+        robot.delay(robot.getAutoDelay());
+        robot.keyRelease(key);
+    }
+
     static void click(Robot robot, ButtonTool...keys) {
         for (ButtonTool key : keys) {
             key.click(robot);
         }
     }
 
-    static void clicks(Robot robot, Special...keys) {
-        for (Special key : keys) robot.keyPress(key.getKey());
+    static void press(Robot robot, ButtonTool...keys) {
+        for (ButtonTool key : keys) {
+            key.press(robot);
+        }
+    }
+
+    static void release(Robot robot, ButtonTool...keys) {
+        for (ButtonTool key : keys) {
+            key.release(robot);
+        }
+    }
+
+    static void clicks(Robot robot, Option...keys) {
+        for (Option key : keys) robot.keyPress(key.getKey());
         robot.delay(robot.getAutoDelay());
         for (int i = keys.length - 1; i >= 0; i--) {
             robot.keyRelease(keys[i].getKey());
